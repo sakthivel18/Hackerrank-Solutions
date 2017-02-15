9c9255bf36ec5a9553e6e8d42fb5dad9ee7e5955
Sakthi <sakthivelravichandran18@gmail.com>
2017-02-15 22:43:36 +0530

    Updated PDF viewer

--- Algorithms/Implementation/Design PDF Viewer/PDF_Viewer.java
+++ Algorithms/Implementation/Design PDF Viewer/PDF_Viewer.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class PDF_Viewer {
 	 public static void main(String[] args) {
 	        Scanner in = new Scanner(System.in);
-	        int[] h = {4 ,2 ,1 ,2 ,3 ,4 ,3 ,7 ,4 ,1 ,5 ,6 ,1 ,3 ,2 ,6 ,6 ,3 ,7 ,3 ,1 ,1 ,5 ,1 ,1 ,4};
-	        /*int h[] = new int[n];
+	        int n = 26;
+	        int h[] = new int[n];
 	        for(int h_i=0; h_i < n; h_i++){
 	            h[h_i] = in.nextInt();
-	        }*/
-	        String word = "qjhwkcexec";
+	        }
+	        String word = in.next();
 	        word = word.toUpperCase();
 	        int max = h[0];
 	        for(int i=0;i<word.length();i++)
